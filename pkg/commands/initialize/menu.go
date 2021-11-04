ec79d80d4d0499a99788093dc2b63b4d5823cea5
Elie Perelman <elie@Elies-MacBook-Pro.local>
2021-11-04 20:56:49 +0200

    Task: Add menu option to user details

--- pkg/commands/initialize/menu.go
+++ pkg/commands/initialize/menu.go
@@ -7,6 +7,10 @@ import (
 	"path"
 	"path/filepath"
 
+	"github.com/blinkops/blink-go-cli/gen/client"
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
+
 	"github.com/manifoldco/promptui"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
@@ -23,13 +27,7 @@ func setupMenuOptions(command *cobra.Command, _ []string) (err error) {
 		Label:   "Hostname",
 		Default: "https://app.blinkops.com",
 	}
-
 	fullHostname, err := prompt.Run()
-	if err != nil {
-		return err
-	}
-
-	u, err := url.Parse(fullHostname)
 	if err != nil {
 		return err
 	}
@@ -39,19 +37,49 @@ func setupMenuOptions(command *cobra.Command, _ []string) (err error) {
 			"Blink API Key (Obtain key by accessing %s/api/v1/apikey in your webbrowser)", fullHostname,
 		),
 	}
-
 	apiKey, err := prompt.Run()
 	if err != nil {
 		return err
 	}
 
-	prompt = promptui.Prompt{
-		Label: "Workspace ID",
-	}
-
-	workspaceID, err := prompt.Run()
+	u, err := url.Parse(fullHostname)
 	if err != nil {
 		return err
+	}
+
+	r := httptransport.New(u.Host, client.DefaultBasePath, []string{u.Scheme})
+	r.DefaultAuthentication = httptransport.Compose(
+		httptransport.APIKeyAuth("BLINK-API-KEY", "header", apiKey),
+	)
+
+	userDetails, err := client.New(r, strfmt.Default).UserInfo.GetUserDetails(nil, nil)
+	if err != nil {
+		// handle this error and prompt the user for manual?
+		return err
+	}
+
+	var workspaces []string
+
+	for _, val := range userDetails.Payload.Workspaces {
+		workspaces = append(workspaces, val.Name)
+	}
+
+	promptSelect := promptui.Select{
+		Label: "Workspace ID",
+		Items: workspaces,
+	}
+
+	_, workspaceName, err := promptSelect.Run()
+	if err != nil {
+		return err
+	}
+
+	// look up the workspace id
+	var workspaceID string
+	for key, val := range userDetails.Payload.Workspaces {
+		if val.Name == workspaceName {
+			workspaceID = key
+		}
 	}
 
 	createConfigFile()
