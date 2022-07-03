7c4ed094455009f83ac48b5305b1c2fdd5572d2b
Daniel Kuperman <daniel@blinkops.com>
2022-07-03 19:45:05 +0300

    Daniel-Kuperman/TSK-4310-run-tests-on-every-pr-to-blink-cli (#34)

---
+++ test/backwards_compatible/cli_tests.sh
@@ -0,0 +1,64 @@
+# uncomment these for local debugging:
+#BLINK_USER_API_KEY=<your-blink-api-key>
+#E2E_CTRL_ADDR=<your controller addr (e.g https://app.dev.blinkops.com)>
+
+# helper vars for constructing blink cmds
+addr=${E2E_CTRL_ADDR#https://} # blink cli expects a url without schema
+flags="--api-key $BLINK_USER_API_KEY --hostname $addr --workspace 55555555-5555-5555-5555-555555555555"
+
+# remove this line to test the script locally without docker
+alias blink="docker run --rm -v $(pwd)/test/backwards_compatible:/inputs blinkops/blink-cli"
+# change to "." to test the script locally without docker
+inputs_dir="/inputs" #inputs_dir="."
+
+# fails the script in case a command has failed
+assert() { # $1: test case description
+  if [ $? -ne 0 ]
+  then
+    echo
+    echo "$1 failed" >&2
+    exit 1
+  fi
+}
+
+# tests begin here
+echo
+echo "create automations:\n"
+automation_id=$(blink playbooks create -f $inputs_dir/cli_automation_case_1.yaml -p "test pack" $flags)
+assert "create automation"
+echo $automation_id
+automation_id2=$(blink playbooks create -f $inputs_dir/cli_automation_case_2.yaml -p "test pack" $flags)
+assert "create automation"
+echo $automation_id2
+
+echo
+echo "execute automation:\n"
+execution_id=$(blink playbooks execute --id $automation_id $flags)
+assert "execute automation"
+echo $execution_id
+
+execution_id=$(echo $execution_id | egrep '[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}' -o)
+assert "egrep uuid from execution"
+echo $execution_id
+
+echo
+echo "get execution:\n"
+blink executions get --id $execution_id $flags
+assert "get automation"
+
+echo
+echo "update automation:\n"
+blink playbooks update -f $inputs_dir/cli_automation_case_1_update.yaml $flags
+assert "update automation"
+
+echo
+echo "delete automations:\n"
+blink playbooks delete --id $automation_id $flags
+assert "delete automation"
+
+echo
+blink playbooks delete --id $automation_id2 $flags
+assert "delete automation"
+
+unalias blink
+
